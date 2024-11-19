dca593f3536432fbe162f6c8620cd8f3c007ed24
Jeyeon Kim <wpdus2694@naver.com>
2024-11-19 16:56:43 +0900

    [Silver V] Title: 영화감독 숌, Time: 284 ms, Memory: 84828 KB -BaekjoonHub

---
+++ Java/백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int num = 666;
+		int count = 1;
+		while(count != N) {
+			num ++;
+			if(Integer.toString(num).contains("666")) count ++;
+		}
+		System.out.print(num);
+	}
+}
