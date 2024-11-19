c4bf63fc983c3554166bf9c9ca5d8007aade8e79
Jeyeon Kim <wpdus2694@naver.com>
2024-11-19 15:19:00 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 104 ms, Memory: 14104 KB -BaekjoonHub

---
+++ Java/백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        String[] input = br.readLine().split(" ");
+        int n = Integer.parseInt(input[0]);
+        int m = Integer.parseInt(input[1]);
+        System.out.println(n > m ? ">" : (n < m ? "<" : "=="));
+    }
+}
