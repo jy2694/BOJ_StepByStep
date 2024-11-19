a0498991c331e4c767e73cde81672664467e081b
Jeyeon Kim <wpdus2694@naver.com>
2024-11-19 15:10:25 +0900

    [Bronze V] Title: 나머지, Time: 104 ms, Memory: 14200 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        String[] input = br.readLine().split(" ");
+        int A = Integer.parseInt(input[0]);
+        int B = Integer.parseInt(input[1]);
+        int C = Integer.parseInt(input[2]);
+        System.out.println((A+B)%C);
+        System.out.println(((A%C) + (B%C))%C);
+        System.out.println((A*B)%C);
+        System.out.println(((A%C)*(B%C))%C);
+    }
+}
