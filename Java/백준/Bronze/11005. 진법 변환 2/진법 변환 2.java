42154439058b85556dd2256a8574f89d9a9a2811
Jeyeon Kim <wpdus2694@naver.com>
2024-11-19 17:16:42 +0900

    [Bronze I] Title: 진법 변환 2, Time: 108 ms, Memory: 14272 KB -BaekjoonHub

---
+++ Java/백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        System.out.println(Long.toString(Long.parseLong(st.nextToken()), Integer.parseInt(st.nextToken())).toUpperCase());
+    }
+}
